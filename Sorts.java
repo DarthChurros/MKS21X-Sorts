d957d1bbb61a64487add60fd1a5280264e115e48
DarthChurros <tnarayan00@stuy.edu>
2018-12-04 19:21:05 -0500

    improved selectionSort by switching only for the minimum

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,15 @@
  public class Sorts {
   public static void selectionSort(int[] ary) {
-    for (int i = 0; i < ary.length; i++)
-      for (int j = i + 1; j < ary.length; j++)
-        if (ary[i] > ary[j]) {
-          int temp = ary[i];
-          ary[i] = ary[j];
-          ary[j] = temp;
+    for (int i = 0; i < ary.length; i++) {
+    int min = i;
+      for (int j = i + 1; j < ary.length; j++) {
+        if (ary[j] < ary[min]) {
+          min = j;
         }
+        int temp = ary[min];
+        ary[min] = ary[j];
+        ary[j] = temp;
+      }
+    }
   }
 }
